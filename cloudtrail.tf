1dadbdafd8465367519a9867f5e2ac4835d7b52a
root <root@DESKTOP-S7GGDE6.localdomain>
2021-06-06 01:21:01 -0300

    Added workflow

---
+++ cloudtrail.tf
@@ -0,0 +1,44 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_cloudtrail" "trail-cloud" {
+  name                          = "tf-cloudmista-trail"
+  s3_bucket_name                = aws_s3_bucket.foo.id
+  s3_key_prefix                 = "prefix"
+  include_global_service_events = false
+}
+
+resource "aws_s3_bucket" "s3-cloud" {
+  bucket        = "tf-cloudzmista-trail"
+  force_destroy = true
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailAclCheck",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:GetBucketAcl",
+            "Resource": "arn:aws:s3:::tf-cloudzmista-trail"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::tf-cloudzmista-trail/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "cloudtrail_name" {
+  default = "cloudtrail-01"
+}
+
+
+variable "s3_bucketname" {
+
+  default = "cloudztrails3"
+
+
+}
